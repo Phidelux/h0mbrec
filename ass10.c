40d377ff44802aaa681d986a081d5a9298c628b2
Andreas Schickedanz <as@bornageek.com>
2020-02-06 23:51:46 +0100

    Assignment 10: Initial solution

---
+++ ass10.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int arr[10];
+
+    printf("Size of array is %ld\n", sizeof(arr) / sizeof(int));
+
+    return 0;
+}
